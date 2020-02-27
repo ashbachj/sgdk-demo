4c664c9987ce0479c6a2b5f08309eaed6d9fe257
jason <ashbachj@gmail.com>
2020-02-26 22:56:28 -0600

    Add tile tutorial

---
+++ src/include/tiles.h
@@ -0,0 +1,19 @@
+#ifndef TILES_H_
+#define TILES_H_
+#include <types.h>
+
+#define TILE1	1
+
+const u32 tile[8]=
+{
+		0x00111100,
+		0x01144110,
+		0x11244211,
+		0x11244211,
+		0x11222211,
+		0x11222211,
+		0x01122110,
+		0x00111100
+};
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,86 @@
 #include <genesis.h>
-void main() {
-VDP_init();
-VDP_drawText("Hello World!", 2, 2);
-while(1) {
-VDP_waitVSync();
+#include "include/tiles.h"
+
+void joyHandler( u16 joy, u16 changed, u16 state)
+{
+  if (joy == JOY_1)
+  {
+    // Change state of player based on input
+    if (state & BUTTON_RIGHT)
+    {
+      // move right
+    }
+    else if (state & BUTTON_LEFT)
+    {
+      // move left
+    }
+    else if (state & BUTTON_UP)
+    {
+      // move up
+    }
+    else if (state & BUTTON_DOWN)
+    {
+      // move down
+    }
+    else { // button released
+      if ( (changed & BUTTON_RIGHT) | (changed & BUTTON_LEFT) )
+      {
+        // stop x motion
+      }
+      else if ( (changed & BUTTON_UP) | (changed & BUTTON_DOWN) )
+      {
+        // stop y motion
+      }
+    }
+  }
 }
-return 0;
+
+int main()
+{
+  VDP_init();
+  VDP_drawText("Hello World!", 2, 2);
+  JOY_init();
+  JOY_setEventHandler( &joyHandler );
+
+  VDP_loadTileData( (const u32 *)tile, 1, 1, 0);
+  VDP_setTileMapXY(PLAN_A, 1, 5, 5);
+
+  VDP_setTileMapXY(PLAN_B, TILE_ATTR_FULL(PAL2, 0, 1, 0, TILE1), 6, 5);
+  
+  VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL1, 1, 0, 0, TILE1), 7, 7);
+  VDP_setTileMapXY(PLAN_B, TILE_ATTR_FULL(PAL2, 0, 0, 0, TILE1), 7, 7);
+  VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL1, 0, 0, 0, TILE1), 8, 7);
+  VDP_setTileMapXY(PLAN_B, TILE_ATTR_FULL(PAL2, 1, 0, 0, TILE1), 8, 7);
+  
+  VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL1, 1, 0, 0, 1), 7, 7);
+  VDP_setTileMapXY(PLAN_B, TILE_ATTR_FULL(PAL2, 0, 0, 0, 1), 7, 7);
+  VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL1, 0, 0, 0, 1), 8, 7);
+  VDP_setTileMapXY(PLAN_B, TILE_ATTR_FULL(PAL2, 1, 0, 0, 1), 8, 7);
+ 
+  VDP_fillTileMapRect(PLAN_B, TILE_ATTR_FULL(PAL3, 0, 0, 0, TILE1 ), 12, 12, 8, 8); 
+  
+  while(1) {
+    VDP_fillTileMapRect(PLAN_B, TILE_ATTR_FULL(PAL0, 0, 0, 0, TILE1), 12, 1, 4, 4);
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_fillTileMapRect(PLAN_B, TILE_ATTR_FULL(PAL1, 0, 1, 0, TILE1), 12, 1, 4, 4);
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_fillTileMapRect(PLAN_B, TILE_ATTR_FULL(PAL2, 0, 0, 0, TILE1), 12, 1, 4, 4);
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_fillTileMapRect(PLAN_B, TILE_ATTR_FULL(PAL3, 0, 1, 0, TILE1), 12, 1, 4, 4);
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_waitVSync();
+    VDP_waitVSync();
+  }
+  
+  return 0;
 }
